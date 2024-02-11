22796294f15b580c8dc5573c5afe53680458f52c
_Nik0__ <maciek.f137@gmail.com>
2024-02-11 17:01:32 +0100

    Test - Added every nextbot

--- nnb_possessions/data/nnb_possessions/functions/24/little_gerry_end.mcfunction
+++ nnb_possessions/data/nnb_possessions/functions/24/little_gerry_end.mcfunction
@@ -1,7 +1,7 @@
-kill @e[tag=metal_pipe]
+kill @e[tag=little_gerry]
 stopsound @a[tag=nextbot_timeout] * vibecraft:possession
-tag @a[scores={nn_possesion=1},tag=nextbot_timeout] remove nextbot
-tag @a[scores={nn_possesion=1},tag=nextbot_timeout] remove metal_pipe_z
+tag @a[scores={nn_possesion=24},tag=nextbot_timeout] remove nextbot
+tag @a[scores={nn_possesion=24},tag=nextbot_timeout] remove little_gerry_z
 scoreboard players set @a[tag=nextbot_timeout] nn_possesion 0
 tag @a[tag=nextbot_timeout] remove n_nextbot
 tag @a[tag=nextbot_timeout] remove nextbot_timeout
---
+++ templates/generate_nbb_possessions.php
@@ -0,0 +1,28 @@
+original_list = [
+    "metalic_pipe", "toob", "crying_sans", "pbj", "smile_ghost", "idiot", "ohl", "meem", "nope",
+    "noob", "observer", "benny", "racoon", "fazmug", "donot", "bear5", "blabber",
+    "chilly", "smurf_cat", "intruder", "jet", "darkness", "bear", "litlle_gerry",
+    "winrar", "fan", "boykisser", "subspace_tripmine", "nuke", "nothing", "flash", "osc",
+    "cb", "dragon", "carter", "shroom", "grumbo", "giggle", "rei", "ico", "leorio",
+    "mochu", "jack", "unfortunate", "lolguy", "smiler", "boba", "squewe", "ccat",
+    "andy", "inhabitant", "leoret", "dread"
+]
+
+# Generate the updated command block setup
+for index, item in enumerate(original_list):
+    print(f"#> ------------------ {item} | id {index + 1}\n")
+    print(f"#> Detect and assign possession")
+    print(f"execute at @e[type=minecraft:zombie,tag=nextbot] if entity @e[type=minecraft:snowball,distance=..2.2] run execute at @a[distance=..5.5,sort=nearest,tag=!nextbot] run scoreboard players set @p nn_possesion {index + 1}")
+    print(f"execute at @e[type=minecraft:zombie,tag=nextbot] if entity @e[type=minecraft:snowball,distance=..2.2] run tag @e[type=zombie,limit=1,distance=..1.1] add controled")
+    print(f"execute at @e[type=minecraft:zombie,tag=nextbot] if entity @e[type=minecraft:snowball,distance=..2.2] run kill @e[tag=controled,distance=..1.1,limit=1]")
+    print(f"execute at @a[scores={{nn_possesion={index + 1}}}] run tag @p add nextbot\n")
+
+    print("#> Reset on timeout")
+    print(f"execute if entity @a[scores={{nn_possesion={index + 1}}},tag=nextbot_timeout] run function nnb_possessions:{index + 1}/{item}_end\n")
+
+    print("#> Teleport sprite and assign tags")
+    print(f"execute if entity @a[scores={{nn_possesion={index + 1}}},tag=!nextbot_timeout] run tag @a[scores={{nn_possesion={index + 1}}},tag=!nextbot_timeout] add {item}_z")
+    print(f"execute at @a[tag={item}_z] run tp @e[tag={item}] ~ ~2 ~\n")
+
+    print("#> Screen effects tag")
+    print(f"tag @a[tag={item}_z] add n_nextbot\n\n\n\n")
