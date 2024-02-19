6a27cd3386907dbfbe1399606a658144c1d4ed92
_Nik0__ <maciek.f137@gmail.com>
2024-02-19 04:57:00 +0100

    DP | Add more random                spaces

--- templates/generate_jumpscares.py
+++ templates/generate_jumpscares.py
@@ -22,9 +22,9 @@ unicode_chars = [
 for item, char in zip(original_list, unicode_chars):
     print(f"#> {item}")
     print(f"title @a[tag={item}_d] times 0 50 20")
-    print(f'execute as @a[tag={item}_d] if score @s nn_jumpscare matches 1 run title @s title {{"text":"    {char}"}}')
-    print(f'execute as @a[tag={item}_d] if score @s nn_jumpscare matches 2 run title @s title {{"text":"{char}","color":"red"}}')
-    print(f'execute as @a[tag={item}_d] if score @s nn_jumpscare matches 3 run title @s title {{"text":"{char}    "}}')
+    print(f'execute as @a[tag={item}_d] if score @s nn_jumpscare matches 1 run title @s title {{"text":" {char}","color":"red"}}')
+    print(f'execute as @a[tag={item}_d] if score @s nn_jumpscare matches 2 run title @s title {{"text":"{char}    "}}')
+    print(f'execute as @a[tag={item}_d] if score @s nn_jumpscare matches 3 run title @s title {{"text":"{char}","color":"red"}}')
     print(f'execute as @a[tag={item}_d] if score @s nn_jumpscare matches 3 run scoreboard players add @s nn_jumpscare_run 10')
     print(f'# execute as @a[tag={item}_d] if score @s nn_jumpscare matches 3 run title @s title {{"text":"{char}","color":"red"}}')
     print()
